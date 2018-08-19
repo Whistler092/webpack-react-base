d7702995a33f314b0bf67acf57f3e5c8d57becc1
Whistler092 <iamrmairoo@gmail.com>
2018-08-19 16:07:11 -0500

    add styles

--- src/playlist/components/media.js
+++ src/playlist/components/media.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
+import './media.css';
 
 class Media extends Component{
     render(){
+       /* const styles ={
+            container: {
+                color: '#44546b',
+                cursor: 'pointer',
+                width: 260,
+                border: '1px solid red'
+            }
+        }
+        <div style={styles.container} />*/
         return (
-            <div>
-                <div>
-                    <img
-                        src=""
+            <div className="Media" >
+                <div className="Media-cover"> 
+                    <img className="Media-image"
+                        src="./images/covers/bitcoin.jpg"
                         alt=""
-                        width={260} height={160}
+                        width={260} 
+                        height={160}
                         />
                 </div>
-                <h3>¿Por qué aprender React</h3>
-                <p>Por Ramiro Bedoya</p>
+                <h3 className="Media-title" >¿Por qué aprender React?</h3>
+                <p className="Media-autor">Por Ramiro Bedoya</p>
             </div>
         )
     }
