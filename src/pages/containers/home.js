dcb1f4f028aefa104fec446e257fe6a1caa5bd85
Whistler092 <iamrmairoo@gmail.com>
2018-08-19 20:49:39 -0500

    modificadas las categorias

--- src/pages/containers/home.js
+++ src/pages/containers/home.js
@@ -1,10 +1,14 @@
 import React , { Component }from 'react';
 import HomeLayout from '../components/home-layout';
+import Categories from '../../categories/components/categories';
+
 
 class Home extends Component { 
     render() {
         return (
-            <HomeLayout />
+            <HomeLayout >
+                <Categories categories={this.props.data.categories} />
+            </HomeLayout>
         )
     }
 }
--- src/playlist/components/playlist.js
+++ src/playlist/components/playlist.js
@@ -1,25 +1,14 @@
 import React from 'react';
 import Media from './media.js';
 import './playlist.css'
-import Play from '../../icons/components/play';
 
 function Playlist (props) {
-    const categories = props.data.categories;
     return (
         <div>
-            <Play size={100} color="red"></Play>
         {
-            categories.map((category) => {
-                return <div className="Playlist" key={category.id}>
-                    <h2>{category.title}</h2>
-                    <h3>{category.description}</h3>
-                    {
-                        category.playlist.map((item) => {
-                            return <Media key={item.id}
-                                            {...item} />
-                        })
-                    }
-                </div>
+            props.playlist.map((item) => {
+                        return <Media key={item.id}
+                                        {...item} />
             })
         }
         </div>
