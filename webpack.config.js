116190184483be14b195c1299cbf8f0db404edc3
Whistler092 <iamrmairoo@gmail.com>
2018-08-20 15:50:48 -0500

    First pages commit

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,7 @@ module.exports = (env) => {
     output: {
       path: path.resolve(__dirname, 'dist'),
       //filename: 'js/[name].[hash].js',
-      filename: 'js/[name].js',
+      filename: 'js/[name].[hash].js',
       publicPath: path.resolve(__dirname, 'dist')+"/",
       chunkFilename: 'js/[id].[chunkhash].js',
     },
